e552d7103658fc4b018445171ef83baf763975fe
Rahul Dey <rahuldey832000@gmail.com>
2024-05-04 23:05:48 +0530

    Add db context registration

---
+++ PackIT.Infrastructure/Data/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PackIT.Infrastructure.Data.Contexts;
+
+namespace PackIT.Infrastructure.Data;
+
+internal static class Extensions
+{
+    public static IServiceCollection AddSqlite(this IServiceCollection services, IConfiguration  configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Default");
+
+        services.AddDbContext<AppReadDbContext>(option => option.UseSqlite(connectionString));
+        services.AddDbContext<AppWriteDbContext>(option => option.UseSqlite(connectionString));
+
+        return services;
+    }
+}
